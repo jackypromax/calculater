5005d20d29bbe4166590efe3c23aa82f9195ac22
jackypromax <87260478+jackypromax@users.noreply.github.com>
2021-08-14 11:06:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 def maincalculator(num1,operator,num2=0):
+ 
+# findig square 
 
  if operator == "**" and "square":
     squa = num1**2
     print(squa)
-
+    
+#finding square root
  elif operator == "1/2" and "Square root":
     sqr = num1*1/2
     print(sqr)
@@ -11,16 +14,20 @@ def maincalculator(num1,operator,num2=0):
  else:
     num2 = int(input("Enter the second number: "))
 
-
+#adding two numbers
  if operator == "+" and "add":
      ad = num1+num2
      print(ad)
+  #subtracting two number
  if operator == "-" and "Sub":
     su = num2-num1
     print(su)
+  #dividing two number
  if operator == "/" and "divide":
     did = num2/num1
     print(did)
+   
+  #multiply two number
  if operator == "*" and "Multiply":
     mul = num1*num2
     print(mul)
